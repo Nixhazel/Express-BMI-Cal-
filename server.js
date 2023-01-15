94013dc7205901e4f72ce724137fe0ad6edf128f
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2023-01-15 16:08:30 +0100

    added post and get on server to calculate the BMI

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    
+    res.sendFile(__dirname + "/index.html");
 });
-
+app.post("/", (req, res) => {
+    let weight = parseFloat(req.body.weight);
+    let height = parseFloat(req.body.height);
+    let bmi = weight / (height * height);
+    res.send(`Your BMI is ${bmi}`);
+});
 
 app.listen(3000, () => {
     console.log("server started on port 3000");
